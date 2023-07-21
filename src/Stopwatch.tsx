e5842776f10ce859ba975ee75b8968cfb269928f
hugh <hugh@prosopo.io>
2023-07-21 14:35:18 +0100

    Adding MS, fixing title and favicon

--- src/Stopwatch.tsx
+++ src/Stopwatch.tsx
@@ -1,53 +1,80 @@
-import { useState, useEffect } from "react";
-import { Button, Box, Typography, List, ListItem } from "@mui/material";
+import { useState, useEffect, useRef } from "react";
+import {
+    Button,
+    Box,
+    Typography,
+    List,
+    ListItem,
+    ListItemText,
+} from "@mui/material";
 import * as React from "react";
 
-const formatTime = (time: number) => {
+const formatTime = (time: number, ms: number) => {
     const getTwoDigits = (num: number) => `0${num}`.slice(-2);
 
     const hours = getTwoDigits(Math.floor(time / 3600));
     const minutes = getTwoDigits(Math.floor((time % 3600) / 60));
     const seconds = getTwoDigits(time % 60);
 
-    return `${hours}:${minutes}:${seconds}`;
+    return `${hours}:${minutes}:${seconds}.${ms ?? 0}`;
 };
 
 const Stopwatch: React.FC = () => {
-    const [time, setTime] = useState(0);
-    const [isRunning, setIsRunning] = useState(false);
     const [laps, setLaps] = useState<number[]>([]);
+    const [isRunning, setIsRunning] = useState(false);
+    const timerRef = useRef<number>();
+    const startTimeRef = useRef<number>();
+    const lapTimeRef = useRef<number>();
+    const timerDisplayRef = useRef<HTMLHeadingElement>(null);
 
     useEffect(() => {
-        let interval: ReturnType<typeof setInterval>;
-
         if (isRunning) {
-            interval = setInterval(() => {
-                setTime((prevTime) => prevTime + 1);
-            }, 1000);
+            startTimeRef.current = performance.now();
+            lapTimeRef.current = performance.now();
+            timerRef.current = requestAnimationFrame(update);
+        } else {
+            cancelAnimationFrame(timerRef.current ?? 0);
         }
-
-        return () => {
-            if (interval) {
-                clearInterval(interval);
-            }
-        };
+        return () => cancelAnimationFrame(timerRef.current ?? 0);
     }, [isRunning]);
+
+    const update = (timestamp: number) => {
+        const elapsed = Math.floor(
+            (timestamp - (startTimeRef.current ?? 0)) / 1000
+        );
+        const ms = Math.floor((timestamp - (startTimeRef.current ?? 0)) % 1000);
+        if (timerDisplayRef.current) {
+            timerDisplayRef.current.textContent = formatTime(elapsed, ms);
+        }
+        timerRef.current = requestAnimationFrame(update);
+        document.title = formatTime(elapsed, ms);
+    };
 
     const start = () => setIsRunning(true);
     const stop = () => setIsRunning(false);
     const reset = () => {
         setIsRunning(false);
-        setTime(0);
+        if (timerDisplayRef.current) {
+            timerDisplayRef.current.textContent = formatTime(0, 0);
+        }
         setLaps([]);
     };
+
     const lap = () => {
-        const lastLapTime = laps.length > 0 ? laps[laps.length - 1] : 0;
-        setLaps([...laps, time - lastLapTime]);
+        const lapTime = performance.now();
+        const lapSeconds = Math.floor(
+            (lapTime - (lapTimeRef.current ?? 0)) / 1000
+        );
+        const ms = Math.floor((lapTime - (lapTimeRef.current ?? 0)) % 1000);
+        lapTimeRef.current = lapTime;
+        setLaps([...laps, lapSeconds]);
     };
 
     return (
         <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
-            <Typography variant="h2">{formatTime(time)}</Typography>
+            <Typography variant="h2" ref={timerDisplayRef}>
+                {formatTime(0)}
+            </Typography>
             <Box sx={{ display: "flex", gap: 2 }}>
                 <Button variant="contained" onClick={start}>
                     Start
@@ -65,7 +92,10 @@ const Stopwatch: React.FC = () => {
             <List>
                 {laps.map((lap, i) => (
                     <ListItem key={i}>
-                        Lap {i + 1}: {formatTime(lap)}
+                        <ListItemText
+                            primary={`Lap ${i + 1}`}
+                            secondary={formatTime(lap)}
+                        />{" "}
                     </ListItem>
                 ))}
             </List>
